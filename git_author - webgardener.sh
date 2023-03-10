c3c237fc87597143b8ff95b7c0d03c7cc9620967
vladyslav0511 <vladyslavdev1105@gmail.com>
2023-03-10 18:58:37 +0900

    update

---
+++ git_author - webgardener.sh
@@ -0,0 +1,8 @@
+git filter-branch --env-filter '
+	CORRECT_NAME="web-gardener"
+	CORRECT_EMAIL="zhulishen0331@gmail.com"
+	    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+	    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+	    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+	    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+	' -f --tag-name-filter cat -- --branches --tags
